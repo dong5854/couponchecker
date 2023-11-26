16cebb449b370c0ed61524991bc73f688c5c5213
dong young <dong5854@gmail.com>
2023-11-26 18:33:58 +0900

    feat: add google oauth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  throw new Error('Google client ID or client secret not defined in environment variables');
+}
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret:  GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+})
+
+export {handler as GET, handler as POST}
