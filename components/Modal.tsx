1fea728f7d1686011ca90766655e97b16100dc09
dong young <dong5854@gmail.com>
2023-12-03 20:58:22 +0900

    add await to useCoupon

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,6 +7,15 @@ import { useRouter } from 'next/navigation'
 export default function Modal({pageId, status}) {
   const [isOpen, setIsOpen] = useState(false)
   const router = useRouter()
+  const useCouponHandler = async() => {
+    if (isOpen) {
+      setIsOpen(false)
+      await useCoupon(pageId)
+      router.refresh()
+      return
+    }
+    setIsOpen(true)
+  }
   return (
     <>
     {status ? (
@@ -51,14 +60,10 @@ export default function Modal({pageId, status}) {
           </div>
           <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             <button type="button" 
-            onClick={() => {
-              useCoupon(pageId)
-              setIsOpen(false)
-              router.refresh()
-            }}
+            onClick={useCouponHandler}
             className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">사용</button>
             <button type="button" 
-            onClick={() => setIsOpen(false)}
+            onClick={useCouponHandler}
             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">취소</button>
           </div>
         </div>
