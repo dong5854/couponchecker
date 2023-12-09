8ae76209f1a686520e154f2192fe6bed6a07b1fd
dong young <dong5854@gmail.com>
2023-12-09 23:46:51 +0900

    chore: edit dropdown message

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -8,8 +8,8 @@ const DropDown = ()=> {
             <summary className="m-1 btn">전체</summary>
              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                 <li><a>전체</a></li>
-                <li><a>미사용 쿠폰만</a></li>
-                <li><a>사용한 쿠폰만</a></li>
+                <li><a>미사용 쿠폰</a></li>
+                <li><a>사용한 쿠폰</a></li>
             </ul>
         </details>
     )
