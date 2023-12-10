34d66abc175e16f74d441b2808beac4a32b7e1d3
dong young <dong5854@gmail.com>
2023-12-11 00:09:42 +0900

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,19 @@ export const metadata: Metadata = {
   description: '쿠폰을 공유하고 사용을 체크하세요',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-          <body className={`flex h-screen flex-col justify-between font-sans mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 ${inter.className}`}>{children}</body>
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" />
+      <body
+        className={`mx-auto flex h-screen max-w-3xl flex-col justify-between px-4 font-sans sm:px-6 xl:max-w-5xl xl:px-0 ${inter.className}`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
